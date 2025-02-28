0263d3bdcbcbc733da720d9a960e6ee4ac263109
Hassan Talha <156108287+HassanT701@users.noreply.github.com>
2025-02-28 01:12:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ customers = [f"{row['CustomerId']} - {row['Surname']}" for _, row in df.iterrows
 models = {
     "XGBoost Feature Engineered": pickle.load(open('models/xgboost-featureEngineered.pkl', "rb")),
     "XGBoost SMOTE": pickle.load(open('models/xgboost-SMOTE.pkl', "rb")),
-    "Voting Classifier": pickle.load(open('models/voting_clf.pkl', "rb"))
+    #"Voting Classifier": pickle.load(open('models/voting_clf.pkl', "rb"))
 }
 
 # Hugging Face API configuration
